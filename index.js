cfe9ee524189cb9f1820052669897c4cabf8ed0c
Stavroula Guerrero <4stacyg@optonline.net>
2017-10-17 15:39:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,17 +10,53 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
-}
+  var itemName = item;
+  var itemPrice = Math.floor(Math.random() * 100 + 1);
+  var itemObject = { [itemName]: itemPrice };
+  cart.push(itemObject);
+  console.log(itemName + ` has been added to your cart.`);
+  return cart;
+ }
+
 
 function viewCart() {
-  // write your code here
-}
+  if (!cart.length) {
+    return console.log("Your shopping cart is empty.")
+  }
+    const itemsWithPrices = [];
 
-function total() {
-  // write your code here
-}
+    for (let i = 0; i < cart.length; i++) {
+      var itemNames = Object.keys(cart[i]);
+      var itemName = itemNames[0];
+      var itemPrice = cart[i][itemName]
 
+      itemsWithPrices.push(`${itemName} at $${itemPrice}`)
+    }
+
+    if (itemsWithPrices.length === 1) {
+      return console.log(`In your cart, you have ${itemsWithPrices.join()}.`)
+    } else if (itemsWithPrices.length === 2) {
+      return console.log(`In your cart, you have ${itemsWithPrices[0]} and ${itemsWithPrices.slice(-1)}.`)
+    } else {
+      return console.log(`In your cart, you have ${itemsWithPrices.slice(0,-1).join(', ')}, and ${itemsWithPrices.slice(-1)}.`)
+    }
+
+  }
+
+
+    function total() {
+      var totalPrice = [];
+      for (var i = 0; i < cart.length; i++) {
+        var cartObject = cart[i];
+        var itemName = Object.keys(cartObject)[0];
+        var itemPrice = cartObject[itemName];
+        totalPrice.push(itemPrice);
+      }
+      var currentTotal = totalPrice.reduce(function(a, b) {
+        return a + b;
+      }, 0);
+      return currentTotal;
+    }
 function removeFromCart(item) {
   // write your code here
 }
