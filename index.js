ecd4b0d46797889db0747e887d01d7fe9b2708a6
Stavroula Guerrero <4stacyg@optonline.net>
2017-10-17 16:52:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -68,8 +68,8 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if (!cardNumber){
-return console.log("Sorry, we don't have a credit card on file for you.")
+  if (!cardNumber) {
+    return console.log("Sorry, we don't have a credit card on file for you.")
 }
 else {
   console.log("Your total cost is $${cartTotal}, which will be charged to the card ${cardNumber}.")
