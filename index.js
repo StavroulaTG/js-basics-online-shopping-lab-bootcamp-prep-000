f9281579ceff83ca249d8639196a8f57824c6aca
Stavroula Guerrero <4stacyg@optonline.net>
2017-10-17 16:47:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -72,5 +72,5 @@ function placeOrder(cardNumber) {
 return console.log("Sorry, we don't have a credit card on file for you.")
 }
 else {
-return console.log("Your total cost is $${cartTotal}, which will be charged to the card ${cardNumber}.")
+  console.log("Your total cost is $${cartTotal}, which will be charged to the card ${cardNumber}.")
 }
