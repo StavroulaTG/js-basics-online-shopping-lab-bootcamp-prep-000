95b51be4097448c6f87b8d7c3159aaf4dec30184
Stavroula Guerrero <4stacyg@optonline.net>
2017-10-17 16:25:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -63,9 +63,10 @@ function removeFromCart(item) {
     if (cart[i].hasOwnProperty(item)) {
       cart.splice(i, 1);
       return cart;
-   }
-     return console.log("That item is not in your cart.");
-   }
+    }
+  }
+  return console.log("That item is not in your cart.");
+}
 
 function placeOrder(cardNumber) {
   // write your code here
